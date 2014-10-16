5b97e5eba8f02e02928583e3fd7019cd4b67b9c1
Nicola Orritos <nicola.orritos@gmail.com>
2014-10-16 22:59:34 +0200

    Initial release: already working but lacks docs and auto-tests

---
+++ test/progenic_test.js
@@ -0,0 +1,10 @@
+// global describe,it */
+/* 'use strict';
+var assert = require('assert'),
+  progenic = require('../lib/progenic.js');
+
+describe('progenic node module.', function() {
+  it('must be awesome', function() {
+    assert( progenic.awesome(), 'awesome');
+  });
+}); */
